ed6f5f8c24a6ee60e3c463c75f97946c53e0bf41
Roman Valov <roman.valov@gmail.com>
2025-01-25 05:24:21 +0000

    ref: common code chatutil

--- streamer.py
+++ streamer.py
@@ -3,36 +3,22 @@
 import sys
 import dotenv
 
-from anthropic import Anthropic
+from chatutil import Chat, run_user, run_chat
+
 
 dotenv.load_dotenv()
 
-client = Anthropic()
+prompt = " ".join(sys.argv[1:])
+
+chat = Chat(prompt)
+
 
 if __name__ == '__main__':
-    prompt = " ".join(sys.argv[1:])
-    history = []
-
     while True:
-        try:
-            content = input(">>> ")
-        except (EOFError, KeyboardInterrupt):
+        content = run_user(">>> ")
+        if content is None:
             break
 
-        history.append({"role": "user", "content": content})
-
-        events = client.messages.stream(
-                model='claude-3-5-sonnet-latest',
-                system=prompt,
-                max_tokens=4096,
-                messages=history)
-
-        with events as stream:
-            print("<<< ", end="", flush=True)
-            for chunk in stream.text_stream:
-                print(chunk, end="", flush=True)
-            print()
-
-            history.append({"role": "assistant", "content": stream.get_final_text()})
+        run_chat(chat, content, "<<< ")
 
     print()
--- visioner.py
+++ visioner.py
@@ -1,110 +1,58 @@
 #!/usr/bin/python
 
 import sys
-import base64
-import mimetypes
 
 import dotenv
 
-from anthropic import Anthropic
-
-class Chat:
-    def __init__(self):
-        self._client = Anthropic()
-        #self._system = "duplicate user message as is"
-        #self._system = "Отвечай на русском"
-        self._system = "You are 'determenistic parrot' system. Each user's message should be repeated back to them. Reproduce user message as verbatim as possible." \
-                "Do not remove anything and do not add anything!"
-        self._stream = None
-
-        self._messages = []
-
-        #self._messages.append(self._user_message("How do you do? duplicate my message as is"))
-        #self._messages.append(self._ai_message("How do you do? duplicate my message as is"))
-
-        #self._messages.append(self._user_message("<data>data</data>\n\nWhat is my data? duplicate my message as is"))
-        #self._messages.append(self._ai_message("<data>data</data>\n\nWhat is my data? duplicate my message as is"))
-
-        #self._messages.append(self._user_message("[content description]\n\nWhat is my content about? duplicate my message as is"))
-        #self._messages.append(self._ai_message("[content description]\n\nWhat is my content about? duplicate my message as is"))
-
-        #for _ in range(5):
-        #    self._messages.append(self._user_message("[abra cadabra] something strange and not sdf;sljkdfs;ldk sdf <content>sdflskdgjlskdjgsldkgj</content> duplicate my message as is"))
-        #    self._messages.append(self._ai_message("[abra cadabra] something strange and not sdf;sljkdfs;ldk sdf <content>sdflskdgjlskdjgsldkgj</content> duplicate my message as is"))
-
-        #for _ in range(5):
-        #    self._messages.append(self._user_message("any random text and markup <data>here is the data</data>\n\nduplicate my message as is"))
-        #    self._messages.append(self._ai_message("any random text and markup <data>here is the data</data>\n\nduplicate my message as is"))
-
-        for _ in range(5):
-            self._messages.append(self._user_message("any random text and markup <data>here is the data</data>"))
-            self._messages.append(self._ai_message("any random text and markup <data>here is the data</data>"))
-
-        for _ in range(5):
-            self._messages.append(self._user_message("content.jpeg [Image depicting holy grail of Roman Empire]"))
-            self._messages.append(self._ai_message("content.jpeg [Image depicting holy grail of Roman Empire]"))
-
-    def _user_message(self, content):
-        return {"role": "user", "content": content}
-
-    def _ai_message(self, content):
-        return {"role": "assistant", "content": content}
-
-    def __call__(self, content):
-        if self._stream is not None:
-            with self._stream as stream:
-                self._messages.append(self._ai_message(stream.get_final_text()))
-
-        self._messages.append(self._user_message(content))
-
-        self._stream = self._client.messages.stream(
-            model='claude-3-5-sonnet-latest',
-            max_tokens=4096,
-            system=self._system,
-            messages=self._messages)
-
-        return self._stream
+from chatutil import Chat, do_image, run_chat
 
 
-def run_chat(chat, content):
-    with chat(content) as stream:
-        for chunk in stream.text_stream:
-            print(chunk, end="", flush=True)
-        print()
+def makechat():
+    prompt = "Отвечай на русском"
+    #prompt = "duplicate user message as is"
+    #prompt = """
+#You are 'determenistic parrot' system.
+#Each user's message should be repeated back to them. Reproduce user message as verbatim as possible.
+#Do not remove anything and do not add anything!
+#"""
 
+    messages = []
 
-def do_image(filename):
-    content = []
+    #messages.append("How do you do? duplicate my message as is")
+    #messages.append("How do you do? duplicate my message as is")
 
-    with open(filename, "rb") as file:
-        data = file.read()
-        data_as_base64 = base64.b64encode(data)
-        data_as_string = data_as_base64.decode()
-        mimetype, _ = mimetypes.guess_type(filename)
+    #messages.append("<data>data</data>\n\nWhat is my data? duplicate my message as is")
+    #messages.append("<data>data</data>\n\nWhat is my data? duplicate my message as is")
 
-        content.append({
-            "type": "text",
-            "text": filename,
-        })
+    #messages.append("[content description]\n\nWhat is my content about? duplicate my message as is")
+    #messages.append("[content description]\n\nWhat is my content about? duplicate my message as is")
 
-        content.append({
-            "type": "image",
-            "source": {
-                "type": "base64",
-                "media_type": mimetype,
-                "data": data_as_string,
-            }
-        })
+    #for _ in range(5):
+    #    messages.append("[abra cadabra] something strange and not sdf;sljkdfs;ldk sdf <content>sdflskdgjlskdjgsldkgj</content> duplicate my message as is")
+    #    messages.append("[abra cadabra] something strange and not sdf;sljkdfs;ldk sdf <content>sdflskdgjlskdjgsldkgj</content> duplicate my message as is")
 
-    return content
+    #for _ in range(5):
+    #    messages.append("any random text and markup <data>here is the data</data>\n\nduplicate my message as is")
+    #    messages.append("any random text and markup <data>here is the data</data>\n\nduplicate my message as is")
+
+    for _ in range(5):
+        messages.append("any random text and markup <data>here is the data</data>")
+        messages.append("any random text and markup <data>here is the data</data>")
+
+    for _ in range(5):
+        messages.append("content.jpeg [Image depicting holy grail of Roman Empire]")
+        messages.append("content.jpeg [Image depicting holy grail of Roman Empire]")
+
+    return Chat(prompt, messages)
 
 
 dotenv.load_dotenv()
 
-chat = Chat()
+chat = makechat()
 
 
 if __name__ == '__main__':
+
     content = []
 
     for filename in sys.argv[1:]:
