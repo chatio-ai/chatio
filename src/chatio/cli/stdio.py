e79e82e02974cade8f3c0892e8dc9d2b17c60015
Roman Valov <roman.valov@gmail.com>
2025-06-09 08:18:00 +0000

    ref: conditionally setup history on run user input

--- src/chatio/cli/stdio.py
+++ src/chatio/cli/stdio.py
@@ -7,17 +7,28 @@ from contextlib import suppress
 from .style import Style, Empty
 
 
-HISTORY_FILE = '.chatio_history'
+class SetupHistory:
+    _HISTORY_FILE = '.chatio_history'
+
+    def __init__(self):
+        self._done = False
+
+    def __call__(self):
+        self.do_setup()
+
+    def do_setup(self):
+        if self._done:
+            return
+
+        with suppress(FileNotFoundError):
+            readline.read_history_file(self._HISTORY_FILE)
+
+        atexit.register(readline.write_history_file, self._HISTORY_FILE)
+
+        self._done = True
 
 
-def setup_history():
-    with suppress(FileNotFoundError):
-        readline.read_history_file(HISTORY_FILE)
-
-    atexit.register(readline.write_history_file, HISTORY_FILE)
-
-
-setup_history()
+setup_history = SetupHistory()
 
 
 def _mk_style(style=None):
@@ -45,6 +56,7 @@ def run_info(chat, style=None, file=None):
 
 
 def run_user(style=None, file=None):
+    setup_history()
     with _mk_style(style).wrap_input(end="", file=file) as prompt:
         try:
             return input(prompt)
