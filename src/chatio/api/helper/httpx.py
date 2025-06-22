e25a45196580ea215a7be55b5be7afc14446955d
Roman Valov <roman.valov@gmail.com>
2025-06-22 15:19:13 +0000

    add: conditional http streaming output event trace

--- src/chatio/api/helper/httpx.py
+++ src/chatio/api/helper/httpx.py
@@ -1,6 +1,8 @@
 
 import os
 
+from collections.abc import Iterator
+
 import httpx
 
 
@@ -15,9 +17,33 @@ def _log_response(response: httpx.Response) -> None:
     print(f"{response.http_version} {response.status_code}\n{headers}\n\n")
 
 
+class LoggedStream(httpx.SyncByteStream):
+    def __init__(self, stream: httpx.SyncByteStream) -> None:
+        self.stream = stream
+
+    def __iter__(self) -> Iterator[bytes]:
+        for chunk in self.stream:
+            print(chunk.decode())
+            yield chunk
+
+
+def _log_response_trace(response: httpx.Response) -> None:
+    _log_response(response)
+    if not isinstance(response.stream, httpx.SyncByteStream):
+        raise TypeError
+    response.stream = LoggedStream(response.stream)
+
+
 def httpx_args() -> dict:
     args: dict[str, object] = {}
 
+    if os.getenv("CHATIO_HTTPX_TRACE"):
+        args.update({
+            "event_hooks": {
+                "request": [_log_request],
+                "response": [_log_response_trace],
+            },
+        })
     if os.getenv("CHATIO_HTTPX_DEBUG"):
         args.update({
             "event_hooks": {
