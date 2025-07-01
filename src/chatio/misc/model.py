e02ac1bf25e54eb7843233379ec1c6d83f11d908
Roman Valov <roman.valov@gmail.com>
2025-07-01 23:28:30 +0000

    fix: parse of model name with double slashes found

--- src/chatio/misc/model.py
+++ src/chatio/misc/model.py
@@ -8,41 +8,48 @@ import tomllib
 from chatio.core.config import ModelConfig
 
 
-def _vendor_config_parse(vendor_path: str) -> tuple[pathlib.Path, pathlib.Path, dict]:
-    _tmp_vendor_path = pathlib.Path(vendor_path)
+def _vendor_config_parse(model_name: str) -> tuple[str, str, dict]:
+    config = None
 
-    _vendors_dir = pathlib.Path("./vendors").resolve()
-    for _vendor_path in _tmp_vendor_path.parents[:-1]:
-        config_file = _vendors_dir.joinpath(_vendor_path.with_suffix('.toml')).resolve()
+    vendors_dir = pathlib.Path('./vendors').resolve()
+    vendor_path = pathlib.Path()
+    while model_name:
+        vendor_path_part, _, model_name = model_name.partition('/')
+        vendor_path = vendor_path.joinpath(vendor_path_part)
+
+        config_file = vendors_dir.joinpath(vendor_path).with_suffix('.toml').resolve()
 
         try:
             with config_file.open('rb') as vendorfp:
                 config_data = tomllib.load(vendorfp)
+
+            _vendor_path = config_file.relative_to(vendors_dir).with_suffix("")
+            config = str(_vendor_path), model_name, config_data
         except FileNotFoundError:
-            continue
-        else:
-            _model_path = _tmp_vendor_path.relative_to(_vendor_path)
-            _vendor_path = config_file.relative_to(_vendors_dir).with_suffix("")
-            return _vendor_path, _model_path, config_data
+            if config:
+                break
 
-    raise FileNotFoundError
+    if config is None:
+        raise FileNotFoundError
+
+    return config
 
 
-def vendor_config(vendor_path: str, config_override: dict | None = None) -> ModelConfig:
+def vendor_config(model_name: str, config_override: dict | None = None) -> ModelConfig:
     if config_override is None:
         config_override = {}
 
     config_client_override = config_override.pop('client', {})
     config_format_override = config_override.pop('format', {})
 
-    config = _vendor_config_parse(vendor_path)
+    config = _vendor_config_parse(model_name)
 
     _vendor_path, _model_name, _config = config
     _config.update(config_override)
 
     vendor_env_ns = _config.get('env_ns')
     if vendor_env_ns is None:
-        vendor_env_ns = _vendor_path.stem
+        vendor_env_ns = _vendor_path.rpartition('/')[-1]
     vendor_env_ns = vendor_env_ns.upper()
 
     _config_client = _config.setdefault('client', {})
@@ -53,7 +60,7 @@ def vendor_config(vendor_path: str, config_override: dict | None = None) -> Mode
     _config_format = _config.setdefault('format', {})
     _config_format.update(config_format_override)
 
-    return ModelConfig(str(_vendor_path), str(_model_name), _config)
+    return ModelConfig(_vendor_path, _model_name, _config)
 
 
 def build_model(model_name: str | None = None, env_ns: str | None = None) -> ModelConfig:
