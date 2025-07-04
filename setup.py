c697a569e1b8aa7c7f468d2c5cd76272faf9c1bd
Roman Valov <roman.valov@gmail.com>
2025-07-04 05:49:20 +0000

    chore: bump version 0.0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="chatio",
-    version="0.0.0",
+    version="0.0.5",
     author="Roman Valov",
     author_email="roman.valov@gmail.com",
     description="LLM API",
