430b840418d49df54904f69b195831a329a5a343
Roman Valov <roman.valov@gmail.com>
2025-07-23 19:53:57 +0000

    ref: keep httpx hooks in class due to flake8 async

--- src/chatio/api/helper/httpx.py
+++ src/chatio/api/helper/httpx.py
@@ -6,47 +6,48 @@ from collections.abc import Iterator
 import httpx
 
 
-def _log_request(request: httpx.Request) -> None:
-    content = request.content.decode()
-    headers = "\n".join(f"{k}: {v}" for k, v in request.headers.items())
-    print(f"{request.method} {request.url}\n{headers}\n\n{content}\n\n")
+class HttpxHooks:
 
+    def log_request(self, request: httpx.Request) -> None:
+        content = request.content.decode()
+        headers = "\n".join(f"{k}: {v}" for k, v in request.headers.items())
+        print(f"{request.method} {request.url}\n{headers}\n\n{content}\n\n")
 
-def _log_response(response: httpx.Response) -> None:
-    headers = "\n".join(f"{k}: {v}" for k, v in response.headers.items())
-    print(f"{response.http_version} {response.status_code}\n{headers}\n\n")
+    def log_response(self, response: httpx.Response) -> None:
+        headers = "\n".join(f"{k}: {v}" for k, v in response.headers.items())
+        print(f"{response.http_version} {response.status_code}\n{headers}\n\n")
 
+    def log_response_trace(self, response: httpx.Response) -> None:
+        self.log_response(response)
+        if not isinstance(response.stream, httpx.SyncByteStream):
+            raise TypeError
 
-def _log_response_trace(response: httpx.Response) -> None:
-    _log_response(response)
-    if not isinstance(response.stream, httpx.SyncByteStream):
-        raise TypeError
+        _iter_bytes = response.iter_bytes
 
-    _iter_bytes = response.iter_bytes
+        def iter_bytes(chunk_size: int | None = None) -> Iterator[bytes]:
+            for chunk in _iter_bytes(chunk_size):
+                print(chunk.decode())
+                yield chunk
 
-    def iter_bytes(chunk_size: int | None = None) -> Iterator[bytes]:
-        for chunk in _iter_bytes(chunk_size):
-            print(chunk.decode())
-            yield chunk
-
-    response.iter_bytes = iter_bytes  # type: ignore[method-assign]
+        response.iter_bytes = iter_bytes  # type: ignore[method-assign]
 
 
 def httpx_args() -> dict:
     args: dict[str, object] = {}
+    hooks = HttpxHooks()
 
     if os.getenv("CHATIO_HTTPX_TRACE"):
         args.update({
             "event_hooks": {
-                "request": [_log_request],
-                "response": [_log_response_trace],
+                "request": [hooks.log_request],
+                "response": [hooks.log_response_trace],
             },
         })
     if os.getenv("CHATIO_HTTPX_DEBUG"):
         args.update({
             "event_hooks": {
-                "request": [_log_request],
-                "response": [_log_response],
+                "request": [hooks.log_request],
+                "response": [hooks.log_response],
             },
         })
 
