7f867c33f8b904b245ef860c52e2f5a318672126
Roman Valov <roman.valov@gmail.com>
2025-07-27 01:21:37 +0000

    ref: support invocation of commands as python mods

--- src/chatio/cli/_cli/__init__.py
+++ src/chatio/cli/_cli/__init__.py
@@ -6,15 +6,21 @@ from typing import Protocol
 
 # pylint: disable=too-few-public-methods
 class EntryPointFunc(Protocol):
-    def __call__(self, *args: str) -> None:
+    def __call__(self, *args: str) -> int | None:
         ...
 
 
 # pylint: disable=too-few-public-methods
 class EntryPoint(Protocol):
-    def __call__(self) -> None:
+    def __call__(self) -> int | None:
         ...
 
 
 def entry_point(func: EntryPointFunc) -> EntryPoint:
-    return lambda: func(*sys.argv[1:])
+    def _func() -> int | None:
+        return func(*sys.argv[1:])
+
+    if func.__module__ == '__main__':
+        raise SystemExit(_func())
+
+    return _func
