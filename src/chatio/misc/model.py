fd2224904bcf0d4a5fa05e20ff13e720aad0efba
Roman Valov <roman.valov@gmail.com>
2025-08-14 13:17:40 +0000

    ref: use ref config field instead of symbolic link

--- src/chatio/misc/model.py
+++ src/chatio/misc/model.py
@@ -10,7 +10,7 @@ from importlib import resources
 from chatio.core.config import ModelConfig
 
 
-def _vendor_config_parse_dir(vendors_root: pathlib.Path, model_name: str) -> tuple[str, str, dict]:
+def _vendor_config_parse_dir(vendors_root: pathlib.Path, model_name: str) -> ModelConfig | None:
     config = None
 
     vendors_dir = vendors_root.resolve()
@@ -25,39 +25,46 @@ def _vendor_config_parse_dir(vendors_root: pathlib.Path, model_name: str) -> tup
             with config_file.open('rb') as vendorfp:
                 config_data = tomllib.load(vendorfp)
 
-            _vendor_path = config_file.relative_to(vendors_dir).with_suffix("")
-            config = str(_vendor_path), model_name, config_data
+            vendor_name = str(config_file.relative_to(vendors_dir).with_suffix(""))
+            config = ModelConfig(vendor_name, model_name, config_data)
         except FileNotFoundError:
-            if config:
+            if config is not None:
                 break
-
-    if config is None:
-        raise FileNotFoundError
 
     return config
 
 
-def _vendor_config_parse(model_name: str) -> tuple[str, str, dict]:
-    config = None
+def _vendor_config_parse(model_name: str) -> ModelConfig | None:
+    vendors_dirs = os.environ.get('CHATIO_VENDORS_PATH')
+    if vendors_dirs is None:
+        vendors_dirs = ""
 
-    vendors_paths = os.environ.get('CHATIO_VENDORS_PATH')
-    if vendors_paths:
-        for vendors_path in vendors_paths.split(':'):
-            try:
-                config = _vendor_config_parse_dir(pathlib.Path(vendors_path), model_name)
-                break
-            except FileNotFoundError:
-                continue
+    for vendors_dir in vendors_dirs.split(':'):
+        vendors_dir_path = pathlib.Path(vendors_dir)
+        config = _vendor_config_parse_dir(vendors_dir_path, model_name)
+        if config is not None:
+            return config
 
-    if config is None:
-        vendors_res = resources.files('chatio').joinpath('share/vendors')
-        with resources.as_file(vendors_res) as vendors_res_dir:
-            config = _vendor_config_parse_dir(vendors_res_dir, model_name)
+    vendors_res = resources.files('chatio').joinpath('share/vendors')
+    with resources.as_file(vendors_res) as vendors_res_path:
+        return _vendor_config_parse_dir(vendors_res_path, model_name)
 
+
+def _vendor_config_fetch(model_name: str) -> ModelConfig:
+    config = _vendor_config_parse(model_name)
     if config is None:
         raise FileNotFoundError
 
-    return config
+    config_ref = config.config.pop('ref', None)
+    if config_ref is None:
+        return config
+    if config.config:
+        raise ValueError
+
+    if not config_ref.startswith('/'):
+        config_ref = config.vendor.rpartition('/')[0] + '/' + config_ref
+
+    return _vendor_config_fetch(config_ref + '/' + config.model)
 
 
 def _vendor_config_merge(config_defaults: dict, config_override: dict) -> dict:
@@ -69,22 +76,21 @@ def _vendor_config_merge(config_defaults: dict, config_override: dict) -> dict:
     return config
 
 
-def _vendor_config_setup(
-        vendor_path: str, config_defaults: dict, config_override: dict) -> dict:
+def _vendor_config_setup(config: ModelConfig, overrides: dict) -> ModelConfig:
 
-    config = _vendor_config_merge(config_defaults, config_override)
+    config.config = _vendor_config_merge(config.config, overrides)
 
-    vendor_env_ns = config.get('env_ns')
+    vendor_env_ns = config.config.get('env_ns')
     if vendor_env_ns is None:
-        vendor_env_ns = vendor_path.rpartition('/')[-1]
+        vendor_env_ns = config.vendor.rpartition('/')[-1]
     vendor_env_ns = vendor_env_ns.upper()
 
-    config['client'] = config.get('client')
-    if config['client'] is None:
-        config['client'] = {}
+    config.config['client'] = config.config.get('client')
+    if config.config['client'] is None:
+        config.config['client'] = {}
 
-    config['client'].setdefault('api_key', os.getenv(f"{vendor_env_ns}_API_KEY"))
-    config['client'].setdefault('base_url', os.getenv(f"{vendor_env_ns}_BASE_URL"))
+    config.config['client'].setdefault('api_key', os.getenv(f"{vendor_env_ns}_API_KEY"))
+    config.config['client'].setdefault('base_url', os.getenv(f"{vendor_env_ns}_BASE_URL"))
 
     return config
 
@@ -102,14 +108,10 @@ def build_model(model_name: str | None = None, env_ns: str | None = None) -> Mod
             raise RuntimeError(err_msg)
 
     env_name = f"{_env_ns}_VENDOR_CONFIG"
-    _config_override = os.environ.get(env_name)
-    config_override = {}
-    if _config_override is not None:
-        config_override = json.loads(_config_override)
+    _overrides = os.environ.get(env_name)
+    overrides = {}
+    if _overrides is not None:
+        overrides = json.loads(_overrides)
 
-    _config_defaults = _vendor_config_parse(model_name)
-    vendor_path, model_name, config_defaults = _config_defaults
-
-    config = _vendor_config_setup(vendor_path, config_defaults, config_override)
-
-    return ModelConfig(vendor_path, model_name, config)
+    config = _vendor_config_fetch(model_name)
+    return _vendor_config_setup(config, overrides)
