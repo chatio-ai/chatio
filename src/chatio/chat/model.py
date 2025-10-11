b7738f85fcdf8370df6b6f8e1c1e861c55a9a670
Roman Valov <roman.valov@gmail.com>
2025-10-11 14:12:35 +0000

    add: dynamic detection and importing of api module

--- src/chatio/chat/model.py
+++ src/chatio/chat/model.py
@@ -1,4 +1,8 @@
 
+from importlib import import_module
+
+import chatio.api
+
 from chatio.core.facade import ApiFacadeDeps
 from chatio.core.facade import ApiFacade
 
@@ -7,27 +11,11 @@ def init_facade(config: dict) -> ApiFacade:
     return ApiFacade(_init_facade_deps(config))
 
 
-# ruff: noqa: PLC0415
-# pylint: disable=import-outside-toplevel
 def _init_facade_deps(config: dict) -> ApiFacadeDeps:
 
     api = config.get('api')
-    match api:
-        case 'claude':
-            from chatio.api.claude.facade import ClaudeFacadeDeps
-            return ClaudeFacadeDeps(config)
-        case 'google':
-            from chatio.api.google.facade import GoogleFacadeDeps
-            return GoogleFacadeDeps(config)
-        case 'openai':
-            from chatio.api.openai.facade import OpenAIFacadeDeps
-            return OpenAIFacadeDeps(config)
-        case 'compat':
-            from chatio.api.compat.facade import CompatFacadeDeps
-            return CompatFacadeDeps(config)
-        case str():
-            err_msg = f"api is not supported: {api}"
-            raise RuntimeError(err_msg)
-        case _:
-            err_msg = "api is not specified"
-            raise RuntimeError(err_msg)
+    if not api:
+        err_msg = "api is not specified"
+        raise RuntimeError(err_msg)
+
+    return import_module(f'.{api}', package=chatio.api.__name__).API(config)
